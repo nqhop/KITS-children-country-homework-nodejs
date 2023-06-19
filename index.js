4dc60d15d70c6616805d409e4d4c05a527ce1af1
Nguyen Quoc Hop <hop35889@gmail.com>
2023-06-19 08:32:54 +0700

    change branch main

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const childrenController = require('./controllers/children')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/childrens', childrenController.getChildrens)
-app.post('/childrens', childrenController.addChildrens)
-app.get('/childrens/:country', childrenController.getChildrensByCountry)
-app.post('/benchmark/:size', childrenController.generate)
-
-
 app.listen(port, () => {
     console.log(`app running on port ${port}`)
 })
