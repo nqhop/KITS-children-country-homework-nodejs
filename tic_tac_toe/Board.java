a4d0f346455111ff485e8ada60cf7c571d9e733e
Nguyen Quoc Hop <hop35889@gmail.com>
2023-06-29 14:48:36 +0700

    tic tac toe

--- tic_tac_toe/Board.java
+++ tic_tac_toe/Board.java
@@ -1,38 +1,74 @@
 package tic_tac_toe;
 
+import java.util.Scanner;
+
 public class Board {
-    int[][] matrix = new int[3][3];
+    int[] matrix = new int[3 * 3];
+    boolean player1InTurn;
+    String name;
 
     public void printMatrix() {
-        for (int[] row : matrix) {
-            for (int i : row) {
-                if (i == 0)
-                    System.out.print("_");
-                else if (i == 1)
-                    System.out.print("X");
-                else
-                    System.out.print("O");
-            }
-            System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            if (i == 3 || i == 6)
+                System.out.println();
+            if (matrix[i] == 0)
+                System.out.print("_");
+            else if (matrix[i] == 1)
+                System.out.print("X");
+            else
+                System.out.print("O");
         }
+        System.out.println();
     }
 
-    public boolean updateMatrix(int row, int column, boolean player1InTurn) {
-        matrix[row][column] = (player1InTurn) ? 1 : 2;
-
-        // check row and column
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2] && matrix[i][0] != 0)
-                return true;
-            if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i] && matrix[0][i] != 0)
+    public boolean check() {
+        for (int i = 0; i < 3; i++) {
+            if (matrix[i] != 0 && matrix[i] == matrix[i + 3] && matrix[i] == matrix[i + 6])
                 return true;
         }
-
-        // check
-        if(matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2] && matrix[0][0] != 0)
+        for (int i = 0; i < 9; i += 3) {
+            if (matrix[i] != 0 && matrix[i] == matrix[i + 1] && matrix[i] == matrix[i + 2])
+                return true;
+        }
+        if (matrix[0] == matrix[4] && matrix[0] == matrix[8] && matrix[0] != 0)
             return true;
-        if(matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0] && matrix[0][2] != 0)
+        if (matrix[2] == matrix[4] && matrix[2] == matrix[6] && matrix[2] != 0)
             return true;
         return false;
+    }
+
+    public void selectPosition(String name1, String nama2) {
+        Scanner myObj = new Scanner(System.in);
+        for(int i = 0; i < matrix.length; i++){
+            if (player1InTurn)
+                name = name1;
+            else
+                name = nama2;
+
+            while (true) {
+                this.printMatrix();
+
+                System.out.print("\n" + name + ": select position from 1 to 9: ");
+                int pos = Integer.valueOf(myObj.nextLine());
+                if (pos > 9 || pos < 1) {
+                    System.out.println("Invalid position");
+                } else if (matrix[pos - 1] != 0) {
+                    System.out.println("Choose another position");
+                } else {
+                    if (player1InTurn)
+                        matrix[pos - 1] = 1;
+                    else
+                        matrix[pos - 1] = 2;
+                    break;
+                }
+            }
+            if(this.check()){
+                this.printMatrix();
+                System.out.println(name + " win");
+                return;
+            }
+            player1InTurn = !player1InTurn;
+        }
+
     }
 }
--- tic_tac_toe/Game.java
+++ tic_tac_toe/Game.java
@@ -1,12 +1,9 @@
 package tic_tac_toe;
 
-import java.util.Scanner;
-
 public class Game {
     private Player player1;
     private Player player2;
     private Board board = new Board();
-    boolean player1InTurn;
 
     public Game(Player player1, Player player2) {
         this.player1 = player1;
@@ -14,27 +11,7 @@ public class Game {
     }
 
     public void go() {
-        while (true) {
-
-            board.printMatrix();
-            Scanner myObj = new Scanner(System.in);
-            if (player1InTurn)
-                System.out.println(player1.getName());
-            else
-                System.out.println(player2.getName());
-            System.out.print("select your row: ");
-            int row = Integer.valueOf(myObj.nextLine()) - 1;
-
-            System.out.print("select your column: ");
-            int column = Integer.valueOf(myObj.nextLine()) - 1;
-
-            if (board.updateMatrix(row, column, player1InTurn)) {
-                System.out.println((player1InTurn) ? player1.getName() : player2.getName() + " win");
-                return;
-            }
-            player1InTurn = !player1InTurn;
-        }
-
+        board.selectPosition(player1.getName(), player2.getName());
     }
 
 }
